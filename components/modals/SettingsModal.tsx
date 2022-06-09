a8a00540671c9223b48327db9ddf27057dc6add7
Nathan Nicholson <thenathannicholson@gmail.com>
2022-06-08 19:45:25 -0500

    Add an animation to the settings modal component

--- components/modals/SettingsModal.tsx
+++ components/modals/SettingsModal.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { motion } from 'framer-motion';
 import AppContext from '../../context/AppContext';
 import styles from '../../styles/Home.module.scss';
 import DurationSettings from '../settings/DurationSettings';
@@ -8,8 +9,18 @@ import ColorSettings from '../settings/ColorSettings';
 export default function SettingsModal() {
   const { setSettingsHidden } = useContext(AppContext);
   return (
-    <div className={styles['settings-bg']}>
-      <section className={styles['settings-container']}>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+      className={styles['settings-bg']}
+    >
+      <motion.section
+        initial={{ y: '100vw', opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+        className={styles['settings-container']}
+      >
         {/* Settings Header */}
         <div className={styles['settings-header']}>
           <h2>Settings</h2>
@@ -26,7 +37,7 @@ export default function SettingsModal() {
         <button type='button' onClick={() => setSettingsHidden(true)}>
           Apply
         </button>
-      </section>
-    </div>
+      </motion.section>
+    </motion.div>
   );
 }
